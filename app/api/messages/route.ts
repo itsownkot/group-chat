ac1cf744a89f9378601cb0fbbafc8ad19779fd6d
itsownkot <rghaari@gmail.com>
2023-10-16 19:53:21 +0700

    add send messages functionality and render them

---
+++ app/api/messages/route.ts
@@ -0,0 +1,78 @@
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { Message } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+const MESSAGES_BATCH = 10;
+
+export async function GET(req: Request) {
+  try {
+    const profile = currentProfile();
+    if (!profile) return new NextResponse("Unauthorized", { status: 401 });
+    const { searchParams } = new URL(req.url);
+
+    const [cursor, channelId] = [
+      searchParams.get("cursor"),
+      searchParams.get("channelId"),
+    ];
+
+    if (!channelId)
+      return new NextResponse("Channel id is missing", { status: 400 });
+
+    let messages: Message[] = [];
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } else {
+      messages = await db.message.findMany({
+        take: MESSAGES_BATCH,
+        where: {
+          channelId,
+        },
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
+
+    let nextCursor = null;
+
+    if (messages.length === MESSAGES_BATCH) {
+      nextCursor = messages[MESSAGES_BATCH - 1].id;
+    }
+
+    return NextResponse.json({
+      items: messages,
+      nextCursor,
+    });
+  } catch (error) {
+    console.log("[MESSAGES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
